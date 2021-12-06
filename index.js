a7ce856f816231f9c4b9d7da8fb583e071ccd99e
Gustavo A <atencio361@hotmail.com>
2021-12-06 00:06:35 -0500

    Open on load

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const $ = selector => document.querySelector(selector)
 const $$ = selector => document.querySelectorAll(selector)
 
-console.log('Hola')
+// setTimeout(() => {
+//   let div
+//   const bot = $('df-messenger')
+//   div = bot.shadowRoot.querySelector('div')
+//   const chat = div.querySelector('df-messenger-chat')
+//   div = chat.shadowRoot.querySelector('div')
+//   const userInput = div.querySelector('df-messenger-user-input')
+//   div = userInput.shadowRoot.querySelector('div')
+//   userInput.shadowRoot.querySelector('div').classList.add('valid')
+//   const input = div.querySelector('input')
+//   console.log(input)
+//   input.value = '¡Hola!'
+// }, 10 * 1000)
